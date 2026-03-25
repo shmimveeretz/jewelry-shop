b64d6d89d533c6847495e7f409cc7306efd02812
unknown <ravivroi7@gmail.com>
2026-03-25 15:08:18 +0200

    Fixed Syntaxes

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -2,11 +2,11 @@ import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 function ScrollToTop() {
-  const { path"name" } = useLocation();
+  const { pathname } = useLocation();
 
   useEffect(() => {
     window.scrollTo(0, 0);
-  }, [path"name"]);
+  }, [pathname]);
 
   return null;
 }
