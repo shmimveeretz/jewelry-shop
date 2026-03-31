d7239b7ac58d463c0199fa73e62ac6680de35de1
unknown <ravivroi7@gmail.com>
2026-03-31 13:53:12 +0300

    testing for bugs

--- src/utils/payPlusService.js
+++ src/utils/payPlusService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
+const API_URL = "https://jewelry-shop-udr7.onrender.com/api";
 
 /**
  * PayPlus Payment Service for Frontend
